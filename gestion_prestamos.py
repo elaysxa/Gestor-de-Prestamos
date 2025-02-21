718fc1ed22fe50ff616bead1665a69207c10d15d
Elaysha <celayshasantiagomorel@gmail.com>
2025-02-21 16:59:32 -0400

    Agregar verificacion de id_unico

--- gestion_prestamos.py
+++ gestion_prestamos.py
@@ -2,17 +2,22 @@ from utilidades import limpiar_pantalla, separador, generador_id_unico, pausar
 import persistencia as ps
 
 def agregar_prestamo():
-
     limpiar_pantalla()
     separador()
     print('Agregar nuevo prestamo')
     separador()
-    ide = generador_id_unico()
-    id = int(ide)
-    print('Identificador: ', ide)
+
+    
+    id = int(generador_id_unico())
+    print('Identificador: ', id)
     nombre = input(('Ingrese el nombre del prestatario: '))
     monto = float(input('Ingrese el monto del prestamo: '))
-    fecha = input(('Ingrese la fecha del prestamo: \n'))
+    fecha = input(('Ingrese la fecha del prestamo: '))
+
+    #Verificar si el Id de prestamo  ya existe
+    while any(d['Id'] == id for d in ps.prestamos):
+        id = int(generador_id_unico())
+
     #Agregar el prestamo al diccionario
     prestamo = {
         'Id': id,
