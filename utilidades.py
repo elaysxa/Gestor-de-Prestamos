fcb35b704c822ba6b1e0c4e93db064514ab43d56
Elaysha <celayshasantiagomorel@gmail.com>
2025-02-21 11:05:02 -0400

    Agregra generador de id unico

--- utilidades.py
+++ utilidades.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 
 def limpiar_pantalla():
     os.system('cls')
@@ -7,4 +9,10 @@ def separador():
     print('-'*40)
 
 def generador_id_unico():
+    #Genera una cadena de string con numeros de 5 digitos
+    caracteres =  string.digits
+    id = ''.join(random.choice(caracteres) for i in range(5))
+    return id
+
+def validar_datos():
     pass
